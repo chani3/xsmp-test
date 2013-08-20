5616e879a405ea46cfe03d1f5c9a2bd900faa2b5
Chani Armitage <chanika@gmail.com>
2013-08-20 09:55:25 -0700

    Add lineedits

--- kde-hello/main.cpp
+++ kde-hello/main.cpp
@@ -4,24 +4,32 @@
 #include <KDebug>
 #include <QLabel>
 #include <KAboutData>
+#include <QVBoxLayout>
+#include <QLineEdit>
 
 int main(int argc, char**argv) {
     kDebug() << "hello world";
     
-    KAboutData ad(
-                         "asdf",
+    KAboutData ad(  "asdf",
                          0,
                          ki18n("asdf"),
-                         "1.0",.
+                         "1.0",
                          ki18n("asdf"),
                          KAboutData::License_GPL,
-                         ki18n("(c) 2007"),);
+                         ki18n("(c) 2007")
+		 );
     
     KCmdLineArgs::init( argc, argv, &ad );
     KApplication app;
     
+    QWidget window;
     QLabel label("hello world");
-    label.show();
+    QLineEdit edit("edit me");
+    QVBoxLayout box(&window);
+    box.addWidget(&label);
+    box.addWidget(&edit);
+    window.show();
+    
     return app.exec();
 }
 
--- qt-hello/main.cpp
+++ qt-hello/main.cpp
@@ -2,12 +2,21 @@
 #include <QApplication>
 #include <QDebug>
 #include <QLabel>
+#include <QVBoxLayout>
+#include <QLineEdit>
 
 int main(int argc, char**argv) {
     qDebug() << "hello world";
     QApplication app(argc, argv);
+    
+    QWidget window;
     QLabel label("hello world");
-    label.show();
+    QLineEdit edit("edit me");
+    QVBoxLayout box(&window);
+    box.addWidget(&label);
+    box.addWidget(&edit);
+    window.show();
+    
     return app.exec();
 }
 
